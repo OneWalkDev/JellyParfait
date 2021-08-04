d5c9e13b6d266aac2b23001f45ac81c9132b5f80
yurisi0212 <dev.yurisi@gmail.com>
2021-08-05 08:49:51 +0900

    youtubeから動画自体をダウンロードせず非同期で音楽を再生できるように

--- JellyParfait/MainWindow.xaml.cs
+++ JellyParfait/MainWindow.xaml.cs
@@ -1,25 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using NAudio.Wave;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using VideoLibrary;
 
 namespace JellyParfait {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        public void exit_click(object sender, RoutedEventArgs e) {
+            Application.Current.MainWindow.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            playMusic("https://www.youtube.com/watch?v=yCl53-3qIwY");
+        }
+        private async void playMusic(string youtubeUrl) {
+            await Task.Run(() => {
+                using (var player = new WaveOutEvent()) {
+                    using (var reader = new MediaFoundationReader(getVideoUrl(youtubeUrl))) {
+                        player.Init(reader);
+                        player.Volume = 0.5f;
+                        player.Play();
+                    }
+                    while (player.PlaybackState == PlaybackState.Playing) {
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+        }
+
+        private string getVideoUrl(string youtubeUrl) {
+            var youTube = YouTube.Default;
+            var video = youTube.GetVideo(youtubeUrl);
+            Debug.Print(video.Uri);
+            return video.Uri;
+
+           
         }
     }
 }
